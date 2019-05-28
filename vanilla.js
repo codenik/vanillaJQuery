501fad1270b75608a34055e22ddd9420f8a8f3e3
Robert <32980360+codenik@users.noreply.github.com>
2019-05-28 16:09:52 +0800

    Update vanilla.js

--- vanilla.js
+++ vanilla.js
@@ -13,3 +13,5 @@ a$$f = (el, attr, val) => a$$(el).filter(el => el[attr] == val);
 a$$f1 = (el, attr, val) => a$$(el).filter(el => el[attr].startsWith(val));
 a$$f5 = (el, attr, val) => a$$(el).filter(el => el[attr].includes(val));
 a$$f9 = (el, attr, val) => a$$(el).filter(el => el[attr].endsWith(val));
+
+robbie$ = 'hello';
